e495f665b13893723da3513d8e7cc17157cd20f5
Sbyte1402 <erick_pasten@outlook.com>
2026-02-25 13:38:37 -0800

    Update: Random Generator implemented

--- Prac3.asm
+++ Prac3.asm
@@ -3,9 +3,9 @@
 ;.INCLUDE "m2560def.inc" ; Incluir definiciones de Registros para 2560
 
 .equ INIT_VALUE = 0 ; Valor inicial R24
-
 ;------------- inicializar ------------------------------------------
-ldi R24,INIT_VALUE
+ldi R24, INIT_VALUE
+LDI R16, 64 ; SEED
 ;------------- implementar ------------------------------------------
 ;call delay20uS
 CALL delay20uS
@@ -65,14 +65,36 @@ delay1S:
             BRNE next4
 RET
 
-arriba: inc R24
-	cpi R24,10
-	breq abajo
-	out PORTA,R24
-	rjmp arriba
+; Xn+1 = (a*Xn + c) mod m -> LCG
+myRand:
+    PUSH R20
+    PUSH R21
+    PUSH R24
+    PUSH R0
+        MOV R20, R16        ; Xn
+        LDI R21, 141        ; a
 
-abajo: dec R24
-	cpi R24,0
-	breq arriba
-	out PORTA,R24
-	rjmp abajo
+        MUL R21, R20        ; [R1:R0] = a*Xn
+
+        MOV R24, R0
+        ADIW R24, 13        ; (a*Xn + c)
+
+        MOV R16, R24        ; R4 = R24 (Reslpado)
+        MOV R25, R0         ; R25 <- R0
+    POP R0
+    POP R24
+    POP R21
+    POP R20
+RET
+
+;arriba: inc R24
+;        cpi R24,10
+;        breq abajo
+;        out PORTA,R24
+;        rjmp arriba
+
+;abajo:  dec R24
+;        cpi R24,0
+;        breq arriba
+;        out PORTA,R24
+;        rjmp abajo
